e49d7840ad918e0476371ff9ab48c36c41f878b1
Ewuji O. John <johnseyi51@gmail.com>
2021-11-11 12:43:02 +0100

    Add project files.

---
+++ Time Logging/Backup.cs
@@ -0,0 +1,56 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Time_Logging
+//{
+//    class Backup
+//    {
+//        Console.WriteLine("Hello World!");
+//            // Creating of File
+//            //Directory.CreateDirectory("c:\\Student_Results");
+//            //var path = @"c:\TimeLogging\TimeLogs.txt";
+//            //File.Create(path);
+//            //Console.WriteLine($"A file has ben created");
+
+//            DateTime[] TimeLog = { };
+
+//        DateTime PresentTime = DateTime.Now;
+            
+//            for (int i = 0; i< 15; i++)
+//            {
+//                PresentTime = TimeLog;
+//            }
+
+//            {
+//                DateTime secValue = DateTime.Parse(dateString);
+//    DateTimeOffset dateOffsetValue = DateTimeOffset.Parse(dateString);
+
+//    //Displaying seconds
+//    Console.WriteLine("Seconds component only: {0}", secValue.ToString("ff"));
+//            }
+
+//Console.WriteLine($"Prent Seconds = {PresentTime}");
+
+//void Calcu()
+//{
+//    for (int i = 0; i < 20; i++)
+//    {
+//        File.AppendAllText(path, "The time in Normal format");
+//        File.AppendAllLines(path, ..............);
+//    }
+
+//}
+
+//static void reversal(Array TimeLog)
+//{
+//    for (int i = 0; i < TimeLog.GetLength(0); i++)
+//    {
+//        File.AppendAllText(path, "The time in Reversedformat");
+//        File.AppendAllLines(path, ..............);
+//    }
+//}
+//    }
+//}
---
+++ Time Logging/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Timers;
+
+namespace Time_Logging
+{
+    class Program
+    {
+        private static System.Timers.Timer aTimer;
+        public static void Main()
+        {
+           
+
+            SetTimer();
+
+            Console.WriteLine("\nPress the Enter key to exit the application...\n");
+            Console.WriteLine("The application started at {0:HH:mm:ss.fff}", DateTime.Now);
+            Console.ReadLine();
+            aTimer.Stop();
+            aTimer.Dispose();
+
+            Console.WriteLine("Terminating the application...");
+        }
+
+        private static void SetTimer()
+        {
+            // Create a timer with a two second interval.
+            aTimer = new System.Timers.Timer(10000);
+            // Hook up the Elapsed event for the timer. 
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+        }
+
+        private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            var path = @"C:\Users\Ewuji O. John\source\repos\Time Logging\TimeLogged.txt";
+            File.WriteAllText(path, "");
+            Console.WriteLine("The Elapsed event was raised at {0:HH:mm:ss.fff}",
+                              e.SignalTime);
+            File.AppendAllLines(path, new String[] { "The Elapsed event was raised at {{0:HH:mm:ss.fff}} " + e.SignalTime } );
+
+        }
+
+        public void GetLoggedTime(string newPath)
+        {
+
+            while (sortedLog.Count > 0)
+            {
+                File.AppendAllLines(newPath, new string[] { sortedLog.Pop().ToString() });
+            }
+            Console.WriteLine("Logging done! Check created file for reversed log.");
+        }
+
+        public string FileMaker()
+        {
+            var path = @"C:\Users\Ewuji O. John\source\repos\Time Logging\TimeLogged.txt";
+            File.Create(path);
+            Console.WriteLine("Folder and file has been Created");
+
+                return path;
+        }
+    }
+}
